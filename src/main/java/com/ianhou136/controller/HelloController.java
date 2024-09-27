a32bb60305080f4b0a3809a440477240c82a9773
Ian Hou <ianhou136@gmail.com>
2024-09-27 18:07:07 +0800

    First Commit

---
+++ src/main/java/com/ianhou136/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.ianhou136.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String Hello(String[] args){
+        System.out.println("Hello World");
+        // return hello world string
+        return "Hello World";
+    }
+
+    @RequestMapping("/simpleParam")
+    public String simpleParam(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String ageStr = request.getParameter("age");
+        return "OK" + name + ageStr;
+    }
+}
